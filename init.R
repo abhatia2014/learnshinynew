ef32c3741921cc5e86328d9e1a31b2612beb005e
Aankur Bhatia <aank2007@gmail.com>
2017-01-19 13:12:12 -0500

    commit more changes

--- init.R
+++ init.R
@@ -1,3 +1,4 @@
 #options(repos = c(CRAN = "http://cran.rstudio.com"))
 install.packages("shiny",repos='https://cran.rstudio.com/',clean = TRUE)
 #install.packages("Rook",clean=T)
+runApp('shiny')
--- startShiny.R
+++ startShiny.R
@@ -1,4 +1,4 @@
-#source("init.R")
+
 runApp('shiny')
 
 
