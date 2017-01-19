2493c608ad50467b4b7a8af47d24e3eedde19667
Aankur Bhatia <aank2007@gmail.com>
2017-01-19 10:44:27 -0500

    commit change

--- init.R
+++ init.R
@@ -1,3 +1,3 @@
 #options(repos = c(CRAN = "http://cran.rstudio.com"))
-install.packages("shiny",quiet = TRUE,clean = TRUE)
+install.packages("shiny",repos='https://cran.rstudio.com/',clean = TRUE)
 install.packages("Rook",clean=T)
